04306015b076d425aa23aaab8518d181f2b3dc83
Shriram <shriram@houseofbluebeans.com>
2019-04-26 10:23:07 +0530

    Revert "Exterior Added"
    This reverts commit 07511311b3d2f6bfd6c38344220ec04e0d9dd07c.

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-exterior",
-      "name": "Exterior",
+      "id": "0-kt07panorama01",
+      "name": "360 Panorama Demo",
       "levels": [
         {
           "tileSize": 256,
@@ -16,27 +16,79 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
         }
       ],
-      "faceSize": 1500,
+      "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": 0.44763680847253795,
-        "pitch": 0.22195986446812732,
-        "fov": 0.947459034658295
+        "yaw": 1.5726183009278119,
+        "pitch": -0.001,
+        "fov": 0.5
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 2.0943720042078073,
+          "pitch": 0.09622162319119276,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG French Door Refrigerator</h1>",
+          "text": "Depth (Including Handles) 34.88\nDepth (Excluding Handles) 32.38\nActual Width (Inches) 35.75\nHeight:Top of Door (Inches) 69.75 "
+        },
+        {
+          "yaw": 1.503868026377714,
+          "pitch": 0.32124298049523325,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG Electric Range</h1>",
+          "text": "Assembled Height (Inches)\n47.125\nWidth (Inches)\n29.875\nDepth (Inches)\n28.25 "
+        },
+        {
+          "yaw": 0.5695713227512584,
+          "pitch": 0.4035265139534481,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG Dishwasher</h1>",
+          "text": "Actual Width (Inches) 23.75\nActual Depth with Door Open 50.5\nDepth Excluding Handle 24.6\nDepth Including Handle 27.125\nMaximum Height 33.6 "
+        },
+        {
+          "yaw": 1.476169585120891,
+          "pitch": -0.15284375053362886,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG Microwave</h1>",
+          "text": "Height (Inches) 16.4375\nWidth (Inches) 29.875\nDepth (Inches) 15.125"
+        }
+      ],
+      "revealHotspots": [
+        {
+          "yaw": 2.22,
+          "pitch": -0.2,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG French Door Refrigerator</h1>",
+          "text": "LG InstaView 26-cu ft French Door Refrigerator with Dual Ice Maker and Door within Door (Stainless Steel) ENERGY STAR",
+          "image_path": "img/048231797061.jpg"
+        },
+        {
+          "yaw": 1.63,
+          "pitch": 0.18,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG Electric Range</h1>",
+          "text": "LG Smooth Surface 5-Element 6.3-cu ft Convection Freestanding Electric Range (Stainless Steel) (Common: 30-in; Actual: 29.875-in)",
+          "image_path": "img/048232330991.jpg"
+        },
+        {
+          "yaw": 0.68,
+          "pitch": 0.2,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG Dishwasher</h1>",
+          "text": "LG 48-Decibel Built-in Dishwasher (Stainless Steel) (Common: 24 Inch; Actual: 23.75-in) ENERGY STAR",
+          "image_path": "img/048231015097.jpg"
+        },
+        {
+          "yaw": 1.618,
+          "pitch": -0.25,
+          "title": "<h1 class=\"h3\" style=\"margin-bottom: 5px; font-size: 1.57143rem; font-weight: 400; color: rgb(51, 51, 51); font-family: &quot;DIN Next&quot;, Helvetica, Arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;\">LG Microwave</h1>",
+          "text": "LG 1.7-cu ft Over-the-Range Microwave (Stainless Steel) (Common: 30-in; Actual: 29.875-in)",
+          "image_path": "img/048232336511.jpg"
+
+        }
+      ]
     }
   ],
-  "name": "Exterior",
+  "name": "360",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
